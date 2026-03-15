706650d7a8dba74798e10439a5c35b314e72d1fb
imMatheus <matheusmendesbarata03@gmail.com>
2026-03-15 12:36:54 -0700

    added scroll reset

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router'
+import { useEffect } from 'react'
 import { Home } from './pages/home'
 import { Provider } from './pages/provider'
 import { Arena } from './pages/arena'
@@ -9,12 +10,21 @@ import { Player } from './components/Player'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 function AppContent() {
   const { pathname } = useLocation()
   const showFooter = pathname !== '/arena'
 
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
