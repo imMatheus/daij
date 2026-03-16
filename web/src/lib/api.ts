cb58fe4c86237c6ed9eb4345e511a4620ff2df97
imMatheus <matheusmendesbarata03@gmail.com>
2026-03-15 23:22:55 -0700

    changed to api endpoint

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3001'
+const API = import.meta.env.VITE_API_URL
 
 export async function fetchJson<T>(path: string): Promise<T> {
   const res = await fetch(`${API}${path}`)
